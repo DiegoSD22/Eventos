46a166e3e56d5d2930912f9d9ea9649e3b00e5da
DiegoSD22 <39284176+DiegoSD22@users.noreply.github.com>
2018-06-05 16:22:54 -0500

    Add files via upload

---
+++ Practica1/Practica1/Captura.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1
+{
+    public partial class Captura : Form
+    {
+        public Captura()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nombre, fecha;
+            int cuenta, diasl, diasn, bono;
+            nombre = textBox1.Text;
+            cuenta = Convert.ToInt16(textBox2.Text);
+            fecha = textBox3.Text;
+            bono = Convert.ToInt16(textBox4.Text);
+            diasl = Convert.ToInt16(textBox5.Text);
+            diasn = Convert.ToInt16(textBox6.Text);
+            float salario = diasl * 700;
+            float bono2 = (bono * salario) / 100;
+            Recibo fr3 = new Recibo();
+            fr3.textBox1.Text = Convert.ToString(salario);
+            fr3.textBox2.Text = Convert.ToString(bono2);
+            float totalp = salario + bono2;
+            fr3.textBox3.Text = Convert.ToString(totalp);
+            float faltas = diasn * 700;
+            fr3.textBox4.Text = Convert.ToString(faltas);
+            float ISR = (salario * 12) / 100;
+            fr3.textBox5.Text = Convert.ToString(ISR);
+            float SS = (salario * 5) / 100;
+            fr3.textBox6.Text = Convert.ToString(SS);
+            float totald = faltas + ISR + SS;
+            fr3.textBox7.Text = Convert.ToString(totald);
+            float neto = totalp - totald;
+            fr3.textBox8.Text = Convert.ToString(neto);
+            fr3.Show();
+        }
+    }
+}
---
+++ Practica1/Practica1/Recibo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1
+{
+    public partial class Recibo : Form
+    {
+        public Recibo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Presentacion fr1 = new Presentacion();
+            fr1.Show();
+        }
+    }
+}
